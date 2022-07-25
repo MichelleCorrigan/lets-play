57c5fbadcf0975bee764439c4aaba54605d5b9ea
lalCorr <lal.corrigan@gmail.com>
2022-07-25 16:19:36 +0000

    Add computer choice function and check result function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,68 @@
 // Add event listeners to the buttons
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
+    let playerChoice = document.getElementById("player-choice");
+    let computerChoice = document.getElementById("computer-choice");
+    let result 
+    let playerScore = document.getElementById("player-score");
+    let computerScore = document.getElementById("computer-score");
+    let choices = ["rock", "paper", "scissors"];
     
+
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            let playerChoice = this.getAttribute("data-choice");
+            playerChoice = this.getAttribute("data-choice");
             runGame(playerChoice);
+            checkResult
         });
     }
 })
 
-let choices = ["rock", "paper", "scissors"];
+function generateComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) [
+        computerChoice = 'rock'
+    ]
+    if (randomNumber === 1) {
+        computerChoice = 'paper'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    computerChoice.innerHTML = computerChoice
+}
 
 
 function runGame(playerChoice) {
 
-    let computorChoice = Math.floor(Math.random() * 3);
-    let result = checkWinner(choices[computorChoice], choices[playerChoice]);    
+    playerChoice.innerHTML = playerChoice
+    
+    console.log(result);
 }
 
 function checkResult() {
-
+    if (computerChoice === playerChoice) {
+        result = "Draw"
+    }
+    if (computerChoice === 'rock' && playerChoice === 'paper') {
+        result = "Player wins"
+    }
+    if (computerChoice === 'rock' && playerChoice === 'scissors') {
+        result = "Computer wins"
+    }
+    if (computerChoice === 'paper' && playerChoice === 'rock') {
+        result = "Computer wins"
+    }
+    if (computerChoice === 'paper' && playerChoice === 'scissors') {
+        result = "Player wins"
+    }
+    if (computerChoice === 'scissors' && playerChoice === 'rock') {
+        result = "Player wins"
+    }
+    if (computerChoice === 'scissors' && playerChoice === 'paper') {
+        result = "Computer wins"
+    }
 }
 
 function incrementPlayerScore() {
