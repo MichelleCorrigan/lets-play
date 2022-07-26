284130c78892f33e403dab9bbeb8bbed47d76002
lalCorr <lal.corrigan@gmail.com>
2022-07-26 16:02:32 +0000

    Get computer choice and result to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,66 @@
 // Wait for the DOM to finish loading before running the game
 // Add event listeners to the buttons
-document.addEventListener("DOMContentLoaded", function() {
+
     let buttons = document.getElementsByTagName("button");
-    let playerChoice = document.getElementById("player-choice");
-    let computerChoice = document.getElementById("computer-choice");
-    let result 
+
+    let playerChoiceDisplay = document.getElementById("player-choice");
+    let computerChoiceDisplay = document.getElementById("computer-choice");
     let playerScore = document.getElementById("player-score");
     let computerScore = document.getElementById("computer-score");
+    let resultDisplay = document.getElementById("result");
     let choices = ["rock", "paper", "scissors"];
+    let computerChoice
     
 
+
+    /* Add event listeners to all buttons */
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            playerChoice = this.getAttribute("data-choice");
-            runGame(playerChoice);
-            checkResult
+            playerChoice = (this.getAttribute(choices));
+            
+            console.log(playerChoice);
+
+            generateComputerChoice();
+            console.log(computerChoice);
+            checkResult();
+            console.log(result);
         });
     }
-})
+
+
 
 function generateComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3);
+     
+    computerChoice = choices[Math.floor(Math.random() * 3)];
+        computerChoiceDisplay.innerHTML = computerChoice
 
-    if (randomNumber === 0) [
-        computerChoice = 'rock'
-    ]
-    if (randomNumber === 1) {
-        computerChoice = 'paper'
-    }
-    if (randomNumber === 2) {
-        computerChoice = 'scissors'
-    }
-    computerChoice.innerHTML = computerChoice
+        playerChoiceDisplay.innerHTML = playerChoice
 }
 
 
-function runGame(playerChoice) {
-
-    playerChoice.innerHTML = playerChoice
-    
-    console.log(result);
-}
-
-function checkResult() {
+function checkResult() { 
     if (computerChoice === playerChoice) {
         result = "Draw"
     }
-    if (computerChoice === 'rock' && playerChoice === 'paper') {
+    else if (computerChoice === 'rock' && playerChoice === 'paper') {
         result = "Player wins"
     }
-    if (computerChoice === 'rock' && playerChoice === 'scissors') {
+    else if (computerChoice === 'rock' && playerChoice === 'scissors') {
         result = "Computer wins"
     }
-    if (computerChoice === 'paper' && playerChoice === 'rock') {
+    else if (computerChoice === 'paper' && playerChoice === 'rock') {
         result = "Computer wins"
     }
-    if (computerChoice === 'paper' && playerChoice === 'scissors') {
+    else if (computerChoice === 'paper' && playerChoice === 'scissors') {
         result = "Player wins"
     }
-    if (computerChoice === 'scissors' && playerChoice === 'rock') {
+    else if (computerChoice === 'scissors' && playerChoice === 'rock') {
         result = "Player wins"
     }
-    if (computerChoice === 'scissors' && playerChoice === 'paper') {
+    else (computerChoice === 'scissors' && playerChoice === 'paper'); {
         result = "Computer wins"
     }
+    resultDisplay.innerHTML = result;
 }
 
 function incrementPlayerScore() {
