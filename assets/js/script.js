a11d2ed12a9bd04aa9011d03d6e378ebca5be42d
lalCorr <lal.corrigan@gmail.com>
2022-07-26 17:34:23 +0000

    Define playerChoice and result

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,36 @@
 // Wait for the DOM to finish loading before running the game
 // Add event listeners to the buttons
 
-    let buttons = document.getElementsByTagName("button");
+    const buttons = document.getElementsByTagName("button");
 
-    let playerChoiceDisplay = document.getElementById("player-choice");
-    let computerChoiceDisplay = document.getElementById("computer-choice");
-    let playerScore = document.getElementById("player-score");
-    let computerScore = document.getElementById("computer-score");
-    let resultDisplay = document.getElementById("result");
-    let choices = ["rock", "paper", "scissors"];
-    let computerChoice
-    
+    const playerChoiceDisplay = document.getElementById("player-choice");
+    const computerChoiceDisplay = document.getElementById("computer-choice");
+    const playerScore = document.getElementById("player-score");
+    const computerScore = document.getElementById("computer-score");
+    const resultDisplay = document.getElementById("result");
+    const choices = ["rock", "paper", "scissors"];
+    let playerChoice;
+    let computerChoice;
+    let result;
 
 
     /* Add event listeners to all buttons */
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            playerChoice = (this.getAttribute(choices));
-            
-            console.log(playerChoice);
-
+            playerChoice = this.getAttribute("data-type"); {
+                if (this.getAttribute("data-type") === "0") {
+                    playerChoice = "rock";
+                } if (this.getAttribute("data-type") === "1") {
+                    playerChoice = "paper";
+                } else {
+                    playerChoice = "scissors";
+                }
+            }
+            playerChoiceDisplay.innerHTML = playerChoice
             generateComputerChoice();
             console.log(computerChoice);
             checkResult();
-            console.log(result);
+            
         });
     }
 
@@ -32,33 +39,32 @@
 function generateComputerChoice() {
      
     computerChoice = choices[Math.floor(Math.random() * 3)];
-        computerChoiceDisplay.innerHTML = computerChoice
-
-        playerChoiceDisplay.innerHTML = playerChoice
+        computerChoiceDisplay.innerHTML = computerChoice;
+            
 }
 
 
 function checkResult() { 
     if (computerChoice === playerChoice) {
-        result = "Draw"
+        result = "Draw";
     }
     else if (computerChoice === 'rock' && playerChoice === 'paper') {
-        result = "Player wins"
+        result = "Player wins";
     }
     else if (computerChoice === 'rock' && playerChoice === 'scissors') {
-        result = "Computer wins"
+        result = "Computer wins";
     }
     else if (computerChoice === 'paper' && playerChoice === 'rock') {
-        result = "Computer wins"
+        result = "Computer wins";
     }
     else if (computerChoice === 'paper' && playerChoice === 'scissors') {
-        result = "Player wins"
+        result = "Player wins";
     }
     else if (computerChoice === 'scissors' && playerChoice === 'rock') {
-        result = "Player wins"
+        result = "Player wins";
     }
-    else (computerChoice === 'scissors' && playerChoice === 'paper'); {
-        result = "Computer wins"
+    else if(computerChoice === 'scissors' && playerChoice === 'paper'); {
+        result = "Computer wins";
     }
     resultDisplay.innerHTML = result;
 }
